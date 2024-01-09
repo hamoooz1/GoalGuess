5146e9f64c0ad68a35d84fd303d89b40589a654b
Hamza Kaderi <kaderihamza4@gmail.com>
2024-01-09 13:55:02 -0800

    make frontend of game

---
+++ client/src/pages/Play.jsx
@@ -0,0 +1,32 @@
+import NavBar from "../components/NavBar";
+import Footer from "../components/Footer";
+import { useState } from "react";
+import "../styles/play.scss";
+
+function Play () {
+
+  const [guessCount, setGuesserCounter] = useState(0)
+
+  return (
+    <>
+    <NavBar />
+    <div className="playing-layout">
+      <h1>Goal Guess</h1>
+      <h2>Premier Soccer Player Guessing Game</h2>
+      <p>{guessCount} of 8 guesses</p>
+    </div>
+    <div className="grid-container">
+      {[...Array(5)].map((_, rowIndex) => (
+        <div key={rowIndex}>
+          {[...Array(6)].map((_, colIndex) => (
+            <div key={colIndex} className="grid-item"> G</div>
+          ))}
+        </div>
+      ))}
+    </div>
+  </>
+
+  )
+}
+
+export default Play;
