0a3dc2f1354f82a0c97ac09a3c08fe2caaa61bff
Aden <aden.mccarville@gmail.com>
2024-01-10 04:24:26 -0500

    Adjust modal styling to allow for multiple modal types to use the same base stylesheet as a baseline

--- client/src/modals/HowToPlayModal.jsx
+++ client/src/modals/HowToPlayModal.jsx
@@ -4,12 +4,13 @@ import howToPlayCircleGreen from "../howToPlayCircleGreen.svg";
 import howToPlayCircleUp from "../howToPlayCircleUp.svg";
 import howToPlayCircleDown from "../howToPlayCircleDown.svg";
 import '../styles/howToPlay.scss';
+import '../styles/modals.scss';
 import closeSymbol from "../closeSymbol.svg";
 
 
 const HowToPlayModal = ({ closeModal, isModalOpen }) => {
   return (
-    <div className="how-to-play-modal">
+    <div className="how-to-play modal">
       <div className="modal-content">
         <button className="how-to-play-modal__close-button" onClick={() => { closeModal(); }}>
           <img src={closeSymbol} alt="close symbol" />
@@ -25,15 +26,14 @@ const HowToPlayModal = ({ closeModal, isModalOpen }) => {
             <li>Use the search bar to guess a name</li>
             <li>After your first guess, you'll get some feedback:</li>
             <div className="modal-circles">
-              <img className="circle one" src={howToPlayCircle} />
               <img className="circle two" src={howToPlayCircleGreen} />
               <img className="circle three" src={howToPlayCircle} />
               <img className="circle four" src={howToPlayCircleGreen} />
               <img className="circle five" src={howToPlayCircleUp} />
               <img className="circle six" src={howToPlayCircleDown} />
             </div>
-            <li>The first four items will turn green if you've correctly guessed the player's Nationality, League, Team, or Position</li>
-            <li>The last two indicate whether the player's age and number are higher or lower</li>
+            <li>The first three items will turn green if you've correctly guessed the player's Nationality, League, Team, or Position</li>
+            <li>The last two indicate whether the player's age and number are higher or lower than that of the player you guessed</li>
           </ul>
         </div>
       </div>
