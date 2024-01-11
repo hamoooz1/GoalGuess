14e442c3e5701d66d557ac98e69ebb996e10a073
Aden <aden.mccarville@gmail.com>
2024-01-11 15:19:42 -0500

    Rollback of branch

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.scss";
 import Homepage from "./pages/Homepage";
-import SignUp from "./pages/SignUp";
-import Login from "./pages/Login";
 import useApplicationData from "./hooks/useApplicationData";
 import HowToPlayModal from "./modals/HowToPlayModal";
 import ModalBackdrop from "./components/ModalBackdrop";
@@ -18,38 +15,16 @@ function App() {
 
   return (
     <div className="App">
-      <Router>
-        <Routes>
-        <Route
-            path="/"
-            element={
-              <Homepage
-                openModal={openModal}
-                isModalOpen={isModalOpen}
-                closeModal={closeModal}
-                modalType={modalType}
-              />
-             }
-          />
-          <Route
-            path="/login"
-            element={
-              <Login/>
-            }
-          />
-          <Route
-            path="/signUp"
-            element={
-              <SignUp/>
-            }
-          />
-        </Routes>
-        {isModalOpen && <ModalBackdrop onClick={closeModal} />}
-        {isModalOpen && modalType === "howToPlay" && <HowToPlayModal closeModal={closeModal} />}
+      <Homepage
+        openModal={openModal}
+        isModalOpen={isModalOpen}
+        closeModal={closeModal}
+        modalType={modalType}
+      />
 
-      </Router>
+      {isModalOpen && <ModalBackdrop onClick={closeModal} />}
+      {isModalOpen && modalType === "howToPlay" && <HowToPlayModal closeModal={closeModal} />}
     </div>
-
   );
 }
 
