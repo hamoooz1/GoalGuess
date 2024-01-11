a83a7f95605153c490a0871bd45549f9e04674b8
Hamza Kaderi <kaderihamza4@gmail.com>
2024-01-10 22:43:08 -0800

    create search bar with db footballers

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -1,28 +1,57 @@
-import React, { useEffect } from "react"
-import { FaSearch } from "react-icons/fa"
-import "../styles/searchbar.scss";
+import React, { useEffect, useState } from "react";
+import { FaSearch } from "react-icons/fa";
 import axios from "axios";
-import { useState } from "react";
+import "../styles/searchbar.scss";
+import PlayerDropdown from "./PlayerDropdown";
 
 export default function SearchBar() {
+  const [footballers, setFootballers] = useState([]);
+  const [selectedFootballer, setSelectedFootballer] = useState({});
+  const [allFootballers, setAllFootballers] = useState([]);
+  const [searchInput, setSearchInput] = useState("");
+  const [showDropdown, setShowDropdown] = useState(false);
 
-  const [footballers, setFootballers] = useState([])
+  const handleInput = (value) => {
+    setSearchInput(value);
+    setShowDropdown(true);
+  };
 
-  
+  const selectFootballer = (player) => {
+    setSelectedFootballer(player);
+  }
 
   useEffect(() => {
-    axios('/api/footballers')
-      .then(response => {
-        setFootballers(response.data);
+    axios
+      .get("/api/footballers")
+      .then((response) => {
+        const results = response.data;
+        setFootballers(results);
+        setAllFootballers(results);
       })
-  }, [])
+      .catch((error) => {
+        console.error("Error fetching footballers:", error);
+      });
+  }, []);
 
-  console.log(footballers)
+  useEffect(() => {
+    const filteredResults = allFootballers.filter((player) =>
+      player.name.toLowerCase().includes(searchInput.toLowerCase())
+    );
+    setFootballers(filteredResults);
+  }, [searchInput, allFootballers]);
 
   return (
+    <>
     <div className="input-wrapper">
       <FaSearch />
-      <input placeholder="Search Player"/>
+      <input
+        onChange={(e) => handleInput(e.target.value)}
+        onFocus={() => setShowDropdown(true)}
+        onBlur={() => setShowDropdown(false)}
+        placeholder="Search Player"
+        />
     </div>
-  )
+    <PlayerDropdown showDropdown={showDropdown} footballers={footballers} selectFootballer={selectFootballer}/>
+      </>
+  );
 }
