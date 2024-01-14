b7d9b32fd24b9f6fad3d5bfb9f3a19a8f03d7a1a
Aden <aden.mccarville@gmail.com>
2024-01-14 14:09:46 -0500

    Refine styling for HowToPlay and WinLoss modal windows

--- client/src/components/HowToPlayModal.jsx
+++ client/src/components/HowToPlayModal.jsx
@@ -2,9 +2,7 @@ import React from "react";
 
 import ModalBackdrop from './ModalBackdrop';
 import howToPlayCircle from "../howToPlayCircle.svg";
-
-
-
+import howToPlayBlank from "../howToPlayBlank.svg";
 import howToPlayCircleGreen from "../howToPlayCircleGreen.svg";
 import howToPlayCircleUp from "../howToPlayCircleUp.svg";
 import howToPlayCircleDown from "../howToPlayCircleDown.svg";
@@ -25,20 +23,27 @@ const HowToPlayModal = ({closeHowToPlayModal}) => {
               How to Play
             </h1>
             <h3>
-              Guess the name of the Premier League player within 6 attempts
+              Guess the player within 6 attempts
             </h3>
             <ul className="modal-list">
-              <li>Use the search bar to guess a name</li>
-              <li>After your first guess, you'll get some feedback:</li>
+              <li>Use the search bar to guess a player</li>
+              <li>After each guess, you'll get some feedback:</li>
               <div className="modal-circles">
                 <img className="circle two" src={howToPlayCircleGreen} />
                 <img className="circle three" src={howToPlayCircle} />
                 <img className="circle four" src={howToPlayCircleGreen} />
+                <img className="circle five" src={howToPlayBlank} />
+                <img className="circle six" src={howToPlayBlank} />
+              </div>
+              <li>The first three boxes turn green if you've guessed the player's Nationality, Team, or Position</li>
+              <div className="modal-circles">
+                <img className="circle two" src={howToPlayBlank} />
+                <img className="circle three" src={howToPlayBlank} />
+                <img className="circle four" src={howToPlayBlank} />
                 <img className="circle five" src={howToPlayCircleUp} />
                 <img className="circle six" src={howToPlayCircleDown} />
               </div>
-              <li>The first three items will turn green if you've correctly guessed the player's Nationality, Team, or Position</li>
-              <li>The last two indicate whether the mystery player's age and number are higher or lower than that of the player you guessed</li>
+              <li>The last two boxes will tell you if the player's age and number are higher or lower than your guess</li>
             </ul>
           </div>
         </div>
--- client/src/components/WinLossModal.jsx
+++ client/src/components/WinLossModal.jsx
@@ -6,21 +6,37 @@ import "../styles/winLoss.scss";
 const WinLossModal = ({ isWinLossModalOpen, closeModal, win, randomFootballer }) => {
   return (
     <>
-        <div className="win-loss modal">
-          <div className="modal-content">
-            <button className="win-loss-modal__close-button" onClick={closeModal}>
-              <img src={closeSymbol} alt="close symbol" />
-            </button>
-            <div className="modal-text">
-              <h1 className="modal-title">
-                {win ? "Great job, you got it!" : "Better luck next time!"}
-              </h1>
-            </div>
-            <p>
-              Close this window to play again!
-            </p>
+      <div className="win-loss modal">
+        <div className="modal-content">
+          <button className="win-loss-modal__close-button" onClick={closeModal}>
+            <img src={closeSymbol} alt="close symbol" />
+          </button>
+          <div className="modal-text">
+            {win ?
+              <>
+                <h1 className="modal-title">
+                  Great job!
+                </h1>
+                <h3>
+                  You correctly guessed the mystery player
+                </h3>
+              </>
+              :
+              <>
+                <h1 className="modal-title">
+                  Out of guesses!
+                </h1>
+                <h3>
+                  Better luck next time
+                </h3>
+              </>
+            }
           </div>
+          <p>
+            Close this window to play again
+          </p>
         </div>
+      </div>
     </>
   );
 };
