7dc5dfb0172b118c0e5a63cd34c471e24d7b234a
Aden <aden.mccarville@gmail.com>
2024-01-17 12:10:41 -0500

    Update WinLoss modal functionality, minor styling adjustments of HowToPlay

--- client/src/components/HowToPlayModal.jsx
+++ client/src/components/HowToPlayModal.jsx
@@ -17,7 +17,7 @@ const HowToPlayModal = ({closeHowToPlayModal}) => {
       <div className="how-to-play modal">
         <div className="modal-content">
           <button className="close-button" onClick={closeHowToPlayModal}>
-            <img src={closeSymbol} alt="close symbol" />
+            <img src={closeSymbol} />
           </button>
           <div className="modal-text">
             <h1 className="modal-title">
@@ -39,7 +39,7 @@ const HowToPlayModal = ({closeHowToPlayModal}) => {
                 <img className="circle five" src={howToPlayCircleUp} />
                 <img className="circle six" src={howToPlayCircleDown} />
               </div>
-              <li>The first three boxes after your guess's photo turn green if you've guessed the mystery player's Nationality, Team, or Position</li>
+              <li>The first three hint boxes will turn green if you've guessed the mystery player's Nationality, Team, or Position</li>
               <div className="modal-circles">
                 <img className="circle one" src={silhouette} />
                 <img className="circle two" src={howToPlayCircleGreen} />
@@ -48,7 +48,7 @@ const HowToPlayModal = ({closeHowToPlayModal}) => {
                 <img className="circle five" src={howToPlayBlank} />
                 <img className="circle six" src={howToPlayBlank} />
               </div>
-              <li>The last two boxes will tell you if the player's age and number are higher or lower than your guess</li>
+              <li>The last two will tell you if the mystery player's age and number are higher or lower than your guess</li>
               <div className="modal-circles">
                 <img className="circle one" src={silhouette} />
                 <img className="circle two" src={howToPlayBlank} />
--- client/src/components/WinLossModal.jsx
+++ client/src/components/WinLossModal.jsx
@@ -1,19 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import closeSymbol from "../closeSymbol.svg";
 import '../styles/modals.scss';
 import "../styles/winLoss.scss";
+import silhouette from "../silhouetteSmall.png";
 
 const WinLossModal = ({ isWinLossModalOpen, closeModal, win, randomFootballer }) => {
+  const [isMysteryPlayerVisible, setIsMysteryPlayerVisible] = useState(false);
 
-
-  console.log("this is the random footballer", randomFootballer)
+  const revealMysteryPlayer = () => {
+    setIsMysteryPlayerVisible(true);
+  };
 
   return (
     <>
       <div className="win-loss modal">
         <div className="modal-content">
           <button className="win-loss-modal__close-button" onClick={closeModal}>
-            <img src={closeSymbol} alt="close symbol" />
+            <img src={closeSymbol} />
           </button>
           <div className="modal-text">
             {win ?
@@ -24,6 +27,10 @@ const WinLossModal = ({ isWinLossModalOpen, closeModal, win, randomFootballer })
                 <h3>
                   You correctly guessed the mystery player
                 </h3>
+                <img src={randomFootballer.image} />
+                <h2>
+                  {randomFootballer.name}
+                </h2>
               </>
               :
               <>
@@ -33,18 +40,30 @@ const WinLossModal = ({ isWinLossModalOpen, closeModal, win, randomFootballer })
                 <h3>
                   Better luck next time
                 </h3>
-                <p>
-                  The mystery player was:
-                </p>
+                <div className="random-footballer">
+                  {isMysteryPlayerVisible ? (
+                    <>
+                      <p>
+                        The mystery player was:
+                      </p>
+                      <img src={randomFootballer.image} />
+                      <h2>
+                        {randomFootballer.name}
+                      </h2>
+                    </>
+                  ) : (
+                    <>
+                      <p>
+                        Click below to reveal the mystery player
+                      </p>
+                      <img src={silhouette} className="win-loss silhouette" onClick={revealMysteryPlayer} />
+                    </>
+                  )}
+                </div>
               </>
             }
           </div>
-          <div className="random-footballer">
-            <img src={randomFootballer.image} />
-            <h2>
-              {randomFootballer.name}
-            </h2>
-          </div>
+
           <p>
             Close this window to play again
           </p>
